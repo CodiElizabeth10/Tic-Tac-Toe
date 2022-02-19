ad15c65afeaa252adedf90d350a2b209a6092179
CodiElizabeth10 <98009702+CodiElizabeth10@users.noreply.github.com>
2022-02-19 16:27:43 -0600

    Update tictactoe.js

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -109,7 +109,7 @@ function checkWinConditions() {
      //sqaures are selected the code executes.
      else if (selectedSquares.length >= 9) {
          //This function plays the tie game sound.
-         audio('/media/tie.mp3"');
+         audio('/media/tie.mp3');
          //This function sets a .3 second timer before the resetGame is called.
          setTimeout(function () { resetGame(); }, 1000);
      
@@ -126,4 +126,20 @@ function arrayIncludes(squareA, squareB, squareC)  {
 //returned and our else if condition executes the drawWinLine function.
 if (a === true && b === true && c === true) { return true }
   }
+}
+
+//this function makes our body element temporarily unclickable
+function disableClick() {
+    //This makes our body unclickable
+    body.style.pointerEvents = 'none';
+    //This makes our body clickable again after 1 second
+    setTimeout(function()  {body.style.pointerEvents = 'auto';}, 1000);
+}
+//this function takes a string parameter of the path you set earlier for 
+//placement sound ('./media/place.mp3')
+function audio (audioURL) {
+    //We create a new audio object and we pass the path as a parameter.
+    let audio = new Audio (audioURL);
+    //play method plays our audio sound.
+    audio.play();
 }
